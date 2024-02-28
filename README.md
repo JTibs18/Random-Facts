22464e49b5185288fdf0ffb1f3d0969ebc0db594
Jessica Tiberio <jessicatiberio18@gmail.com>
2024-02-28 02:43:08 -0500

    api calls

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ To run: flask run
 Resources used: 
 👉 https://youtu.be/_YeN69XoqqU?si=7ZitRpSYmvypDr5-
 👉 https://github.com/newlinedotco/30-days-of-vue
+👉 https://www.youtube.com/watch?v=F_SBxcV335k
+👉 https://rapidapi.com/divad12/api/numbers-1/ 
---
+++ getRandomFact.py
@@ -0,0 +1,24 @@
+import requests
+from constants import API_KEY
+
+def getRandomFact(type, min, max):
+    validTypes = ["trivia", "math", "date", "year"]
+
+    if type not in validTypes:
+        return "Error: invalid type"
+
+    url = f"https://numbersapi.p.rapidapi.com/random/{type}"
+
+    querystring = {"min":min,"max":max,"fragment":"true","json":"true"}
+
+    headers = {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "numbersapi.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    print(response.json())
+    return response.json()
+
+print(getRandomFact("year", 50, 100))
